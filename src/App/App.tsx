727b1115fb1b819c3c29120b3c7ce8ef642d5200
Emily Marigold Klassen <forivall@gmail.com>
2022-03-29 11:56:49 -0700

    feat: initial version working

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,39 +1,118 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react';
-import AppRouting from './AppRouting';
+import { apiKey } from '../config';
+// import AppRouting from './AppRouting';
+
+const styles = './App.module.css';
+
+interface GiphyResults {
+  data: GifObject[];
+}
+
+interface GifObject {
+  id: string;
+  url: string;
+  embed_url: string;
+  images: {
+    preview_gif: {
+      height: number;
+      width: number;
+      url: string;
+    };
+  };
+}
 
 // NOTE: a library should be used in a real-world scenario
-const useFetchData = (): [string?, Error?] => {
-  const [data, setData] = React.useState<string>();
+const useGiphySearch = (term: string): [GiphyResults?, Error?] => {
+  const [data, setData] = React.useState<GiphyResults>();
   const [err, setErr] = React.useState<Error>();
 
   React.useEffect(() => {
     async function fetchData() {
-      const ignorefile = await fetch('/.gitignore');
-      const text = await ignorefile.text();
+      console.log('searching for?', term);
+      if (!term) return;
 
-      setData(text);
+      const results = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${term}`
+      );
+      const json = await results.json();
+
+      setData(json);
     }
     fetchData().catch((err) => {
       setErr(err);
     });
-  }, []);
+  }, [term]);
 
   return [data, err];
 };
 
+const SearchBox: React.VFC<{ onSearch: (value: string) => void }> = (props) => {
+  const { onSearch } = props;
+  const searchInputRef = React.useRef<HTMLInputElement>(null);
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        onSearch(searchInputRef.current?.value ?? '');
+      }}
+    >
+      Search! <input type="search" ref={searchInputRef} />
+      <input type="submit" value="Search!" />
+    </form>
+  );
+};
+
+const SearchResults: React.VFC<{ data: GifObject[] }> = (props) => {
+  const { data } = props;
+  return (
+    <div>
+      {data.map((gifObject) => {
+        return (
+          <span key={gifObject.id}>
+            <img src={gifObject.images.preview_gif.url} />
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
 const App: React.VFC = () => {
-  const [, err] = useFetchData();
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const [results, err] = useGiphySearch(searchTerm);
+
+  const searchBoxEl = (
+    <SearchBox
+      onSearch={(term) => {
+        setSearchTerm(term);
+      }}
+    />
+  );
+
+  if (!results) {
+    return <div>{searchBoxEl}</div>;
+  }
 
   if (err) {
     return (
       <div>
+        {searchBoxEl}
         <span style={{ background: 'red' }}>ERROR:</span>
         {err.message}
       </div>
     );
   }
 
-  return <AppRouting />;
+  return (
+    <div>
+      {searchBoxEl}
+      <SearchResults data={results.data} />
+    </div>
+  );
+
+  // return <AppRouting />;
 };
 
 export default App;
